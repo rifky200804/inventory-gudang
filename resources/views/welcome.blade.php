00a3c83b73c548cc338dd9ac588fc3935b0a77a4
Rifky Syiahbudin <rifkysyiahbudin@Rifkys-MacBook-Pro.local>
2023-06-26 09:16:10 +0700

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    OKE
+                    ASOMASOW
                 </div>
 
                 <div class="links">
