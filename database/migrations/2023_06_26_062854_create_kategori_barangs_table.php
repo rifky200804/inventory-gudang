7c866f558865baa619acc4f1a5e830b5f3f4909e
Rifky Syiahbudin <rifkysyiahbudin@Rifkys-MacBook-Pro.local>
2023-06-28 18:33:29 +0700

    fix migration table

--- database/migrations/2023_06_26_062854_create_kategori_barangs_table.php
+++ database/migrations/2023_06_26_062854_create_kategori_barangs_table.php
@@ -15,7 +15,7 @@ class CreateKategoriBarangsTable extends Migration
     {
         Schema::create('kategori_barangs', function (Blueprint $table) {
             $table->id();
-            $table->string('kode_kategori',100);
+            $table->string('kode_kategori',50);
             $table->string('nama_kategori',100);
             $table->string('created_by',100)->nullable();
             $table->string('updated_by',100)->nullable();
